99a1b3ae5895a6c8ad8c1c1926fc4ca7f7d40a2d
Andrés Botero <0xafbf@gmail.com>
2025-10-12 14:53:28 -0500

    Implemented build scripts for Linux

--- build.sh
+++ build.sh
@@ -1,70 +1,73 @@
+#! /bin/bash
+# Copyright Andrés Botero 2025
+#
+set -ex
 
-# run from repo directory
-source ../emsdk/emsdk_env.sh
-# or in windows
-../emsdk/emsdk_env.ps1
+# first ensure that you have:
+#  - clang
+#  - odin
+#  - cmake
+#  - emsdk install latest
+#  - source emsdk_env
+#  - clone https://github.com/libsdl-org/SDL.git
+#  - cd SDL && git checkout release-3.2.24
+#
 
-cd ../SDL/
-mkdir webbuild; cd webbuild
-emcmake cmake ..
-emmake make # or ninja
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ODIN_PATH="$SCRIPT_PATH/../odin"
+SDL_PATH="$SCRIPT_PATH/../SDL"
 
-cp .\libSDL3.a .\libSDL3.o
-..\odin\odin.exe build src -target:js_wasm32 -define:SDL3_WASM_LIB="c:/abotero/webtest/libsdl3.o"
-
-
-..\odin\odin.exe build src -target:js_wasm32 -build-mode:obj
-
-
-# build with odin, link with emcc
-..\odin\odin.exe build src -target:js_wasm32 -define:SDL3_WASM_LIB="c:/abotero/webtest/libsdl3.o" -build-mode:obj -out:src.wasm.o -define:AB_ENTRY_POINT=true
-emcc .\src.wasm.o .\libSDL3.a -o src.wasm
-
-# build all with odin
-..\odin\odin.exe build src -target:js_wasm32 -define:SDL3_WASM_LIB="c:/abotero/webtest/libsdl3.o" 
+# git clone emsdk
+# ./emsdk install latest
+# ./emsdk activate latest
+# you don't need to source emsdk_env as we do here manually
+EMSDK_PATH="$SCRIPT_PATH/../emsdk"
 
 
-# run wasm-objdump -x "<filename>" | grep "_start" over every file in current folder and tell me what file matches
-for f in *; do
-    if wasm-objdump -x "$f" 2>/dev/null | grep -q '"_start"'; then
-        echo "Match found in: $f"
+
+# ensure that SDL is built for web
+BUILD_PATH="./build"
+PREFIX_PATH="./prefix"
+
+SDL_LIBRARY="./libSDL3.a"
+
+if [ ! -e "$BUILD_PATH/sdl/libSDL3.a" ]; then
+
+    mkdir -p "$BUILD_PATH/sdl"
+    pushd "$BUILD_PATH/sdl"
+
+    if [ -z "$EMSDK" ]; then
+        echo "Loading emsdk environment"
+        source "$EMSDK_PATH/emsdk_env.sh"
     fi
-done
+    emcmake cmake "$SDL_PATH"
+    make "-j$(nproc)"
+    popd
+fi
 
-odin build source/main_web -target:js_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -define:RAYGUI_WASM_LIB=env.o -vet -strict-style -out:$OUT_DIR/game.wasm.o
+echo "Compiling"
+
+"$ODIN_PATH/odin" build src \
+    -target:js_wasm32 \
+    -build-mode:obj \
+    -debug \
+    -out:game.wasm.o \
+#    -show-system-calls \
 
 
+echo "Linking"
 
-..\odin\odin.exe build src -target:freestanding_wasm32
-..\odin\odin.exe build src -target:freestanding_wasm32 -define:AB_FREESTANDING=true
+if [ -z "$EMSDK" ]; then
+    echo "Loading emsdk environment"
+    source "$EMSDK_PATH/emsdk_env.sh"
+fi
 
+emcc \
+    -o "index.html" \
+    "game.wasm.o" \
+    "$BUILD_PATH/sdl/libSDL3.a" \
+    --shell-file "index_template.html" \
+    -sERROR_ON_UNDEFINED_SYMBOLS=0 \
+    -sFETCH \
+    -g
 
-
-PS C:\abotero\webtest> ..\odin\odin.exe build src -target:freestanding_wasm32
-PS C:\abotero\webtest> ..\odin\odin.exe build src -target:freestanding_wasm32 -define:SDL3_WASM_LIB="c:/abotero/webtest/libsdl3.o" -build-mode:obj -out:src.wasm.o                      
-PS C:\abotero\webtest> ..\odin\odin.exe build src -target:freestanding_wasm32 -build-mode:obj -out:src.wasm.o                                                                           
-PS C:\abotero\webtest> rm .\src.wasm.o                                                                                                                                                  
-PS C:\abotero\webtest> ..\odin\odin.exe build src -target:freestanding_wasm32 -build-mode:obj -out:src.wasm.o 
-emcc .\src.wasm.o .\libSDL3.a -o src.wasm
-
--show-system-calls
--print-linker-flags
--extra-linker-flags:arstrast
-
-        -build-mode:<mode>
-                Sets the build mode.
-                Available options:
-                        -build-mode:exe         Builds as an executable.
-                        -build-mode:test        Builds as an executable that executes tests.
-                        -build-mode:dll         Builds as a dynamically linked library.
-                        -build-mode:shared      Builds as a dynamically linked library.
-                        -build-mode:dynamic     Builds as a dynamically linked library.
-                        -build-mode:lib         Builds as a statically linked library.
-                        -build-mode:static      Builds as a statically linked library.
-                        -build-mode:obj         Builds as an object file.
-                        -build-mode:object      Builds as an object file.
-                        -build-mode:assembly    Builds as an assembly file.
-                        -build-mode:assembler   Builds as an assembly file.
-                        -build-mode:asm         Builds as an assembly file.
-                        -build-mode:llvm-ir     Builds as an LLVM IR file.
-                        -build-mode:llvm        Builds as an LLVM IR file.
